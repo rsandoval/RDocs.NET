9de345977a9423f7e54bc2ab03a078c7c3bd081c
Rodrigo Sandoval <rodrigo@rsolver.com>
2019-09-20 20:19:55 -0300

    R:Docs demo 1st version

---
+++ RDocsCoreConsole/Document.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace RDocsCoreConsole
+{
+    public class Document
+    {
+        [LoadColumn(0)]
+        public string ID { get; set; }
+        [LoadColumn(1)]
+        public string Contents { get; set; }
+        [LoadColumn(2)]
+        public int Suptertype { get; set; }
+        [LoadColumn(3)]
+        public string Filename { get; set; }
+        [LoadColumn(4)]
+        public string CalculatedSupertype { get; set; }
+
+        public string Type { get; set; }
+
+    }
+
+    public class DocumentType
+    {
+        [LoadColumn(0)]
+        public string ID { get; set; }
+        [LoadColumn(1)]
+        public string DocumentID { get; set; }
+        [LoadColumn(2)]
+        public int TypeID { get; set; }
+    }
+
+
+
+
+    public class TypePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Type;
+    }
+
+}
---
+++ RDocsCoreConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+
+namespace RDocsCoreConsole
+{
+    class Program
+    {
+        private static string _appPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string _trainDataPath => Path.Combine(_appPath, "C:\tmp", "RDox-Samples.tsv");
+        private static string _trainTypeDataPath => Path.Combine(_appPath, "C:\tmp", "RDox-Samples-Types.tsv");
+        private static string _testDataPath => Path.Combine(_appPath, "C:\tmp", "issues_test.tsv");
+        private static string _modelPath => Path.Combine(_appPath, "C:\tmp", "model.zip");
+
+        private static MLContext _mlContext;
+        private static PredictionEngine<Document, TypePrediction> _predEngine;
+        private static ITransformer _trainedModel;
+        static IDataView _trainingDataView;
+        static IDataView _typesDataView;
+
+        static void Main(string[] args)
+        {
+            _mlContext = new MLContext(seed: 0);
+
+            _trainingDataView = _mlContext.Data.LoadFromTextFile<Document>(_trainDataPath, hasHeader: true);
+            _typesDataView = _mlContext.Data.LoadFromTextFile<Document>(_trainTypeDataPath, hasHeader: true);
+
+            Console.WriteLine("All set!");
+        }
+    }
+}
